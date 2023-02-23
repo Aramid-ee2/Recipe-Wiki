62fa234d7371e242106d4c783165006c677e9b7d
Gabriel Terrazas <gabriel_terrazas@techexchange.in>
2023-02-23 07:17:30 +0000

    Implemented Home Page

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -4,11 +4,15 @@ def make_endpoints(app, backend,logging):
     # Flask uses the "app.route" decorator to call methods when users
     # go to a specific route on the project's website.
     @app.route("/")
-    def home():
+    def main():
         # TODO(Checkpoint Requirement 2 of 3): Change this to use render_template
-        # to render main.html on the home page.
-        logging.info("Someone hit the Home Page")
+        # to render main.html on the main page.
+        logging.info("Someone hit the Main Page")
         return render_template("main.html")
+    # Home Page Route
+    @app.route("/home")
+    def home():
+        return render_template("home.html")        
     # Pages Route
     @app.route("/pages/<page_id>")
     def get_page(page_id):
