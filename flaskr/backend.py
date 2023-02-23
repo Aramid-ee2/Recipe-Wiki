2db19fa01512fad738a69f12ee2f2de926974512
Gabriel Terrazas <gabriel_terrazas@techexchange.in>
2023-02-23 07:22:51 +0000

    Added comments to code

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,9 +1,11 @@
 # TODO(Project 1): Implement Backend according to the requirements.
+# To be able to access the bucket
 from google.cloud import storage
 class Backend:
 
     def __init__(self):
         storage_client = storage.Client()
+        # Initialize access to both buckets
         self.users_bucket = storage_client.bucket('users_project1')
         self.wiki_info_bucket = storage_client.bucket('wiki_info_project1')
         
@@ -17,7 +19,7 @@ class Backend:
         pass
 
     def sign_up(self, user_name, password):
-        # Reach out to GCS and create user with username and password
+        # Reach out to GCS and create user_name file that contains the password
         blob = self.users_bucket.blob(user_name)
         with blob.open("w") as f:
             # TODO: add prefix and hash password
--- flaskr/pages.py
+++ flaskr/pages.py
@@ -1,12 +1,12 @@
+# Imported request to be able to acces info returned from inputs
 from flask import render_template, request
 def make_endpoints(app, backend,logging):
-
     # Flask uses the "app.route" decorator to call methods when users
     # go to a specific route on the project's website.
+    # Default route
     @app.route("/")
     def main():
-        # TODO(Checkpoint Requirement 2 of 3): Change this to use render_template
-        # to render main.html on the main page.
+        # TODO: Log test
         logging.info("Someone hit the Main Page")
         return render_template("main.html")
     # Home Page Route
@@ -16,14 +16,15 @@ def make_endpoints(app, backend,logging):
     # Pages Route
     @app.route("/pages/<page_id>")
     def get_page(page_id):
+        # Returns a different page depending on the page_id input
         return render_template("{}.html".format(page_id))
 
     # Sign up
     @app.route("/sign_up" , methods=['GET', 'POST'])
     def sign_up():
-        # If the request is a Get, then return the page
         # If the request is a Post, get username and password from the request and pass it to backend class
         if request.method == 'POST':
             backend.sign_up(request.form['user_name'], request.form['password'])
+        # If the request is a Get, then return the page
         else:
             return render_template("sign_up.html")
