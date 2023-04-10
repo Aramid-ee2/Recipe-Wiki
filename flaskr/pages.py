c8b7e26d8fdd3f8cfcbcd8be3fb0743d9b5c14ed
Gabriel Terrazas <gabriel_terrazas@techexchange.in>
2023-04-10 23:53:26 +0000

    Removed settings route

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -123,11 +123,3 @@ def make_endpoints(app, backend, logging):
         backend.update_language(request.form["fav_language"])
         settings = backend.get_current_settings()
         return render_template("settings.html", settings = settings)
-
-    #TODO: Test route
-    @app.route("/search", methods=["GET", "POST"])
-    def search():
-        # Add Post
-        return render_template("search.html")
-    
-    # Fix search and add bookmark.html to page
